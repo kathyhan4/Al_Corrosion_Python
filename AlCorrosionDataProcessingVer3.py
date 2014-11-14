50973b8ac8c34730ae21c64331ce27f8de90d73c
kathyhan4 <katherine.han@sunpowercorp.com>
2014-11-13 19:32:01 -0800

    somehow broke datetime again
    now this stupid program is putting an extra " before the month of the
    date.  There is no excuse for this.

--- AlCorrosionDataProcessingVer3.py
+++ AlCorrosionDataProcessingVer3.py
@@ -11,7 +11,7 @@ import os
 from numpy import array
 import numpy
 import datetime
-from datetime import datetime
+#from datetime import datetime
 import time
 import matplotlib.pyplot as plt
 import dateutil
@@ -20,7 +20,7 @@ from pylab import *
 
 rootdir= 'C:\\Users\\khan\\Documents\\Al Corrosion\\Al Corrosion Data DH\\'
 
-DataFile = open(rootdir+'Data_1A_100V_with_temp1_10_24_14_120hr.csv')
+DataFile = open(rootdir+'Data_1A_1000V_with_temp1_10_28_14_120hr.csv')
 data = numpy.recfromcsv(DataFile, delimiter='\t', filling_values=numpy.nan, case_sensitive=True, deletechars='', replace_space=' ')
 
 
@@ -37,7 +37,7 @@ for i in range(len(data)):
     date = data[i][0]
     time2 = data[i][1]
     dateandtime = date + ' ' +  time2
-    date1=datetime.strptime(dateandtime, "%m/%d/%Y %I:%M:%S %p")
+    date1=datetime.datetime.strptime(dateandtime, "%m/%d/%Y %I:%M:%S %p")
     date2 = time.mktime(date1.timetuple())
     timearray.append(date2)
 
@@ -70,15 +70,16 @@ lvlist = numpy.zeros((len(lvlist_length),numbercolumns))
 
 k=0
 l=0
+m=0
 
 for i in range(0,len(timearray)):
    if numarray[i,0] < -50.0:
        for j in range(0,numbercolumns):
           hvlist[k,j] =numarray[i,j]
        k=k+1
-   elif numarray[i,0]>0:
-       for j in range(0,numbercolumns):
-          lvlist[l][j] = numarray[l][j]
+   elif numarray[i,0] > 0:
+       for m in range(0,numbercolumns):
+          lvlist[l][m] = numarray[i][m]
        l+l+1
           
 shapehvlist = hvlist.shape
@@ -134,13 +135,15 @@ for i in range(1,len(hvlist)-1):
    hvlist[i,30] = h0 - hvlist[i,29] #remaining foil thickness in m
    hvlist[i,31] = rhoAl * (L1/hvlist[i,30]/d+L2/h0/d) # ohms calculated before temperature considered
    hvlist[i,32] = (hvlist[i,31] *(1+alphaAl*(hvlist[i,Temperaturecolumn]-20)))*1000 #calculated resistance in m-ohms 
-print hvlist[0,:]
-print hvlist[1,:]
+#print hvlist[0,:]
+#print hvlist[1,:]
 
 plt.plot(hvlist[:,33],hvlist[:,32])
+plt.plot(lvlist[:,33],lvlist[:,2]*1000)
 ylabel('Resistance (m-ohms)')
 xlabel('Time (hrs)')
 title(r'Calculated Resistance')
-show()
+plt.legend(['Calculated', 'Measured'], loc='upper left')
+plt.show()
 
 DataFile.close()
