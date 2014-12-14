02ca7a003c556d25524857c26bf8e681d712404e
kathyhan4 <katherine.han@sunpowercorp.com>
2014-12-14 15:20:46 -0800

    Graphs save proper labels
    Still need to automatically output some data to Excel, unless I can
    process manually easily

--- AlCorrosionDataProcessingVer3_port2_temp1pitting.py
+++ AlCorrosionDataProcessingVer3_port2_temp1pitting.py
@@ -19,20 +19,72 @@ from pylab import *
 
 
 rootdir= 'C:\\Users\\khan\\Documents\\GitHub\\AlCorrosionDataCSVFiles\\'
-#filenamelocation = rootdir+'Data_1A_1000V_with_temp2_11_7_14_480hr_DH_coupon_aqueous_in_port_2_100_and_1000V_for_imaging_11_10_14.csv'
-filenamelocation = rootdir+'Data_1A_500V_with_temp1_10_29_14_120hr.csv'
-#filenamelocation = rootdir+'Data_1A_0V_with_temp1_10_27_14_120hr_control.csv'
-#filenamelocation = rootdir+'Data_1A_100V_with_temp1_10_24_14_120hr.csv'
-#filenamelocation = rootdir+'Data_1A_100V_with_temp1_10_31_14_120hr.csv'
-#filenamelocation = rootdir+'Data_1A_1000V_with_temp1_10_15_14_24hr_new_Al.csv'
-#filenamelocation = rootdir+'Data_1A_1000V_with_temp1_10_22_14_48hr.csv'
-#filenamelocation = rootdir+'Data_1A_1000V_with_temp1_10_28_14_120hr.csv'
-#filenamelocation = rootdir+'Data_1A_1000V_with_temp2_11_3_14_480hr_DH.csv'
-#filenamelocation = rootdir+'Data_1A_1000V_with_temp2_11_7_14_480hr_DH_coupon.csv'
-#filenamelocation = rootdir+'Data_100mA_100V_with_temp1_10_20_14_48hr.csv'
-#filenamelocation = rootdir+'Data_Port1Temp2_DHAl_1000V_Port2Temp1_Cu_Aq_1000V_Port3Temp3Al_100Vimage.csv'
-#filenamelocation = rootdir+'Data_DH_Port1_1000V_Temp2_maybe_more_P2_P3_11-26-14.csv'
+# parameters = [port, thermocouple, length corrosion(L1), length not corrosion (L2), width corrosion (d2), width foil (d), portion pitted, pitting aspect ratio, voltage]
+# Pitting factor: ratio of the depth of the deepest pit resulting from corrosion divided by the average 
+# penetration as calculated from weight loss. - See more at: http://www.nace.org/Pitting-Corrosion/#sthash.ATospuhX.dpuf
 
+## 1 Submerged expt from 10-15-14 port 1 temperature 1 1000V
+#filenamelocation = rootdir+'Data_1A_1000V_with_temp1_10_15_14_24hr_new_Al.csv'
+#parameters = [1, 1, 0.05, 0.06, 0.005, 0.005, 0.5, 1, -1000, 'Submerged']
+
+## 2 Submerged expt from 10-20-14 port 1 temperature 1  100V
+#filenamelocation = rootdir+'Data_100mA_100V_with_temp1_10_20_14_48hr.csv'
+#parameters = [1, 1, 0.05, 0.06, 0.005, 0.005, 0.5, 1, -100, 'Submerged']
+
+## 3 Submerged expt from 10-22-14 port 1 temperature 1  1000V
+### DO NOT USE, FAT ANODE
+#filenamelocation = rootdir+'Data_1A_1000V_with_temp1_10_22_14_48hr.csv'
+#parameters = [1, 1, 0.05, 0.06, 0.005, 0.005, 0.5, 1, -1000, 'Submerged_Fat_Anode']
+
+## 4 Submerged expt from 10-24-14 port 1 temperature 1 100V
+### DO NOT USE, FAT ANODE
+#filenamelocation = rootdir+'Data_1A_100V_with_temp1_10_24_14_120hr.csv'
+#parameters = [1, 1, 0.05, 0.06, 0.005, 0.005, 0.5, 1, -100, 'Submerged_Fat_Anode']
+
+## 5 Submerged expt from 10-27-14 port 1 temperature 1 0V
+### DO NOT USE, FAT ANODE
+#filenamelocation = rootdir+'Data_1A_0V_with_temp1_10_27_14_120hr_control.csv'
+#parameters = [1, 1, 0.05, 0.06, 0.005, 0.005, 0.5, 1, 0, 'Submerged_Fat_Anode']
+
+## 6 Submerged expt from 10-28-14 port 1 temperature 1 1000V
+#filenamelocation = rootdir+'Data_1A_1000V_with_temp1_10_28_14_120hr.csv'
+#parameters = [1, 1, 0.05, 0.06, 0.005, 0.005, 0.5, 1, -1000, 'Submerged']
+
+## 7 Submerged expt from 10-29-14 port 1 temperature 1 500V
+#filenamelocation = rootdir+'Data_1A_500V_with_temp1_10_29_14_120hr.csv'
+#parameters = [1, 1, 0.05, 0.06, 0.005, 0.005, 0.5, 1, -500, 'Submerged']
+
+## 8 Submerged expt from 10-31-14 port 1 temperature 1 100V
+#filenamelocation = rootdir+'Data_1A_100V_with_temp1_10_31_14_120hr.csv'
+#parameters = [1, 1, 0.05, 0.06, 0.005, 0.005, 0.5, 1, -100, 'Submerged']
+
+## 9 DH expt from 11-3-14 port 1 temperature 2 1000V
+#filenamelocation = rootdir+'Data_1A_1000V_with_temp2_11_3_14_480hr_DH.csv'
+#parameters = [1, 2, 0.004, 0.066, 0.005, 0.005, 0.1, 1, -1000, 'DampHeat']
+
+## 10 DH expt from 11-7-14 port 1 temperature 2 1000V
+#filenamelocation = rootdir+'Data_1A_1000V_with_temp2_11_7_14_480hr_DH_coupon.csv'
+#parameters = [1, 2, 0.004, 0.066, 0.005, 0.005, 0.1, 1, -1000, 'DampHeat']
+
+## 11 Submerged expt from 11-10-14 port 2 temperature 1 100V and 1000V
+#filenamelocation = rootdir+'Data_1A_1000V_with_temp2_11_7_14_480hr_DH_coupon_aqueous_in_port_2_100_and_1000V_for_imaging_11_10_14.csv'
+#parameters = [2, 1, 0.05, 0.06, 0.005, 0.005, 0.5, 1, -100]
+
+## 12 Submerged expt from 11-20-14 port 2 temperature 1 1000V Cu strip, tinned
+#filenamelocation = rootdir+'Data_Port1Temp2_DHAl_1000V_Port2Temp1_Cu_Aq_1000V_Port3Temp3Al_100Vimage_from_11_20_14.csv'
+#parameters = [2, 1, 0.05, 0.06, 0.005, 0.005, 0.5, 1, -1000, 'Submerged']
+
+## 13 Submerged expt from 11-20-14 port 3 temperature 3 100V 
+#filenamelocation = rootdir+'Data_Port1Temp2_DHAl_1000V_Port2Temp1_Cu_Aq_1000V_Port3Temp3Al_100Vimage_from_11_20_14.csv'
+#parameters = [3, 3, 0.05, 0.06, 0.005, 0.005, 0.5, 1, -100, 'Submerged']
+
+## 14 DH expt from 11-20-14 port 1 temperature 2 1000V
+#filenamelocation = rootdir+'Data_Port1Temp2_DHAl_1000V_Port2Temp1_Cu_Aq_1000V_Port3Temp3Al_100Vimage_from_11_20_14.csv'
+#parameters = [1, 2, 0.004, 0.066, 0.005, 0.005, 0.1, 1, -1000, 'DampHeat']
+
+## 14 DH expt from 11-7-14 port 1 temperature 2 1000V
+#filenamelocation = rootdir+'Data_DH_Port1_1000V_Temp2_maybe_more_P2_P3_11-26-14.csv'
+#parameters = [1, 2, 0.004, 0.066, 0.005, 0.005, 0.1, 1, -1000, 'DampHeat']
 
 
 
@@ -42,8 +94,8 @@ data = numpy.recfromcsv(DataFile, delimiter='\t', filling_values=numpy.nan, case
 
 size2=len(data[0])
 numbercolumns = size2 + 15
-Port = 1
-Thermocouple = 2
+Port = parameters[0]
+Thermocouple = parameters[1]
 currentcolumn = Port * 3 - 2 #second column for that port's data is current (Amps)
 Temperaturecolumn = Thermocouple + 11
 MeasResColumn = Port * 3 - 1
@@ -51,13 +103,13 @@ MeasCurColumn = Port * 3 - 2
 MeasVoltColumn = Port * 3 - 3
 
 #Set Inputs Specific to this Run
-VoltageBias = -1000 #V
+VoltageBias = parameters[8] #V
 Current4Wire = 1 #1 amp in 4 wire measurement
 OxidationState = 3 #electrons per Al atom in reaction
-L1 = 0.05 #length of corroded area in m
-L2 = 0.06 #length of non-corroded area in m
+L1 = parameters[2] #length of corroded area in m
+L2 = parameters[3] #length of non-corroded area in m
 h0 = 0.000037 #thickness of foil initially in m
-d = 0.005 #width of foil strip in m
+d = parameters[5] #width of foil strip in m
 secondsperpoint = 60
 
 Area1 = 0.15 # Portion of the length of L1 that has deep pitting
@@ -125,7 +177,7 @@ for i in range(0,len(timearray)):
           lvlist[l,m] = numarray[i,m]
        l=l+1
 
-for i in range(1,len(lvlist)-1):
+for i in range(len(lvlist)-1):
    lvlist[i,21] = lvlist[i,20] - lvlist[i-1,20] # difference in time points
    
 lvlist[0,21] = 1
@@ -148,7 +200,17 @@ for i in range(0,len(lvlist)-1):
         Rsumlength = 0
         Restime = 0
         counter1 = counter1 + 1
+        
+for i in range(1,len(AverageRes[:,0])-1):
+    AverageRes[i,2] = (AverageRes[i,0] - AverageRes[0,0])/3600
+    AverageRes[i,3] = ((AverageRes[i,1] - AverageRes[i-1,1])/(AverageRes[i,2] - AverageRes[i-1,2]))*1000
 
+AverageResLen = 0
+
+for i in range(1,len(AverageRes[:,0])-1):
+    if AverageRes[i,2] > 0:
+        AverageResLen = AverageResLen + 1
+   
          
 shapehvlist = hvlist.shape
 shapelvlist = lvlist.shape
@@ -231,26 +293,26 @@ matplotlib.rc('font', **font)
 figure(num=None, figsize=(12, 8), dpi=480, facecolor='w', edgecolor='k')
 plt.plot(hvlistcropped[0:-2,33],hvlistcropped[0:-2,32])
 plt.plot(lvlist[:,33],lvlist[:,MeasResColumn]*1000,'ro')
-plt.plot(lvlist[:,33],lvlist[:,34]*1000,'g')
+#plt.plot(lvlist[:,33],lvlist[:,34]*1000,'g')
 ylabel('Resistance (m-ohms)',**font)
 plt.ylim([0,100])
 #plt.xlim([0,10])
 xlabel('Time (hrs)',**font)
 title(r'Calculated Resistance',**font)
 plt.legend(['Calculated', 'Measured'], loc='upper left')
-savefig(filenamelocation.split('_resistance.')[0]+'.png')
+savefig(filenamelocation.split('_resistance_')[0]+parameters[9]+'.png')
 plt.show()
 
 figure(num=None, figsize=(12, 8), dpi=480, facecolor='w', edgecolor='k')
 
-plt.plot(lvlist[:,33],lvlist[:,35]*1000,'g')
-ylabel('Resistance (m-ohms)',**font)
-plt.ylim([0,100])
+plt.plot(AverageRes[0:AverageResLen,2],AverageRes[0:AverageResLen,3],'g')
+ylabel('Resistance Change (m-ohms/hr)',**font)
+#plt.ylim([0,5])
 #plt.xlim([0,10])
 xlabel('Time (hrs)',**font)
 title(r'Change in Calculated Resistance',**font)
 #plt.legend(['Calculated', 'Measured'], loc='upper left')
-savefig(filenamelocation.split('_delRes.')[0]+'.png')
+savefig(filenamelocation.split('_delRes.')[0]+parameters[9]+'ResistanceChange.png')
 plt.show()
 
 DataFile.close()
